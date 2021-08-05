a5bd32569a99e6e05448d4ed430674f9c0038f29
Panupongy <panupongy62@nu.ac.th>
2021-08-06 03:53:24 +0700

    saiyog Back End Fronnt End

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 const {sequelize} = require('./models')
+let cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
 require('./routes')(app)
 
